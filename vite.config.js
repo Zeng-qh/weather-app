8f51088643e1da136d10a0a3be805543237f04fe
都一样 <976272457@qq.com>
2021-07-06 12:39:53 +0800

    使用vite初始化

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    server: {
+        proxy: {
+            // '/api': {
+            //     target: 'http://jsonplaceholder.typicode.com',
+            //     changeOrigin: true,
+            //     rewrite: (path) => path.replace(/^\/api/, '')
+            // },
+            // 使用 proxy 实例
+            '/api': {
+                target: 'http://jsonplaceholder.typicode.com',
+                changeOrigin: true,
+                configure: (proxy, options) => {
+                    // proxy 是 'http-proxy' 的实例
+                }
+            }
+        }
+    }
+})
